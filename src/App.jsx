11f9424b4731613891cf245d9159ab6a374c2b42
violeta atkinson <violeta.atkinson@gmail.com>
2026-02-15 12:19:09 -0800

    component nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer"
 import Screens from "./components/Screens"
+import Navbar from "./components/Navbar"
 
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Screens/>
       <Footer/>
     </div>
