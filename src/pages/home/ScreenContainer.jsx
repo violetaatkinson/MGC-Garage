514c0de7e345f832b9b7bcc9627409a2b1ce5704
violeta atkinson <violeta.atkinson@gmail.com>
2026-04-09 12:39:53 -0700

    fix imgs vercel 2

--- src/pages/home/ScreenContainer.jsx
+++ src/pages/home/ScreenContainer.jsx
@@ -1,39 +1,46 @@
 import React from "react";
 import ScreenList from "./ScreenList";
 
+
+import t5 from "../../assets/t5.jpeg";
+import fix from "../../assets/fix.jpg";
+import c from "../../assets/c.jpg";
+import b from "../../assets/b.jpg";
+import t from "../../assets/t.jpeg";
+
 const screensInfo = [
 	{
 		id: 1,
 		section: "Inventory",
-		img: "../../assets/t5.jpeg",
+		img: t5,
 		href: "/motos",
 		information: "Browse our collection of motorcycles",
 	},
 	{
 		id: 2,
 		section: "Fixer Upper",
-		img: "../../assets/fix.jpg",
+		img: fix,
 		href: "/fixer-upper",
 		information: "Restore your old bike",
 	},
 	{
 		id: 3,
 		section: "Schedule Service",
-		img: "../../assets/c.jpg",
+		img:c,
 		href: "/schedule-service",
 		information: "Expert repairs and maintenance ",
 	},
 	{
 		id: 4,
 		section: "Parts Request",
-		img: "../../assets/b.jpg",
+		img: b,
 		href: "/parts-request",
 		information: "Source genuine parts for your motorcycle",
 	},
 	{
 		id: 5,
 		section: "Contact Us",
-		img: "../../assets/t.jpeg",
+		img: t,
 		href: "/contact",
 		information: "Get in touch with our team",
 	},
