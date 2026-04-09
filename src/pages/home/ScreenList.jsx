00f74096577cfb3d6f5221b47e3b7db7844364db
violeta atkinson <violeta.atkinson@gmail.com>
2026-04-09 13:45:06 -0700

    loader screens

--- src/pages/home/ScreenList.jsx
+++ src/pages/home/ScreenList.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Screen from "./Screen";
+import Loader from "../../components/Loader"
 
 // envio cada objeto individual 
 const ScreenList = ({ screens }) => {
+	
+	if (!screens || screens.length === 0) {
+		return <Loader />;
+	}
+
 	return (
 		<div className="flex flex-col ">
 			{screens.map((screen) => {
